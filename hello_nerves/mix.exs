cdc93e9692de2b7db1119afff9cb6c8b5263a010
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2023-12-18 18:34:59 +0900

    ch18: add circuits_gpio,_i2c for deps

--- hello_nerves/mix.exs
+++ hello_nerves/mix.exs
@@ -46,6 +46,8 @@ defmodule HelloNerves.MixProject do
       {:shoehorn, "~> 0.9.1"},
       {:ring_logger, "~> 0.10.0"},
       {:toolshed, "~> 0.3.0"},
+      {:circuits_gpio, "~> 1.0"},
+      {:circuits_i2c, "~> 1.0"},
 
       # Allow Nerves.Runtime on host to support development, testing and CI.
       # See config/host.exs for usage.
