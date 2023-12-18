9e56e31f7408126ab223636c9649d4cb4fb54224
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2023-12-18 19:12:50 +0900

    add definition of JSON resouce as routing

--- phoenix_aht20_server/lib/phoenix_aht20_server_web/router.ex
+++ phoenix_aht20_server/lib/phoenix_aht20_server_web/router.ex
@@ -21,9 +21,11 @@ defmodule PhoenixAht20ServerWeb.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", PhoenixAht20ServerWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", PhoenixAht20ServerWeb do
+    pipe_through :api
+
+    resources "/values", ValueController, except: [:new, :edit]
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:phoenix_aht20_server, :dev_routes) do
